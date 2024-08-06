9e58a3e588e85f71ba2e70a091306e410520c463
prachisuryawanshi31766 <suryawanshiprachi0405.com>
2024-08-06 21:29:53 +0530

    9:29 PM - 8/6/2024

---
+++ lecture_11/01_ClassCode/DataDefinationInJS/script.js
@@ -0,0 +1,9 @@
+
+
+let num=10;
+var inum;
+
+var title=document.querySelector("h1");
+var heading2=document.querySelector("h2");
+
+title.textContent=num;
---
+++ lecture_11/02_assignment/DataDefinationInJS/script.js
@@ -0,0 +1,10 @@
+
+
+let ino1=100;
+var ino=50;
+
+var title=document.querySelector("h1");
+var title=document.querySelector("h2");
+
+
+title.textContent=ino1;
