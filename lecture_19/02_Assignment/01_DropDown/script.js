7a7aa55ae6641fb5dd053345768341ab6564e586
prachisuryawanshi31766 <suryawanshiprachi0405.com>
2024-09-23 22:45:12 +0530

    10:45 PM -23/9/2024

---
+++ lecture_19/02_Assignment/01_DropDown/script.js
@@ -0,0 +1,19 @@
+
+const dropdownBtn=document.querySelector(".dropdown_button");
+
+const dropdownList=document.querySelector(".dropdown_list");
+
+function toggleDropdown()
+{
+    dropdownList.classList.toggle("hide_element")
+}
+
+dropdownBtn.addEventListener("click",function(event)    
+{
+
+    event.stopPropagation();
+    toggleDropdown();
+    
+    //alert(dropdownList.classList)
+});
+
