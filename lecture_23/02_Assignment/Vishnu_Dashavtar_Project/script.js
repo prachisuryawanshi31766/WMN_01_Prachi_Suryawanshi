836d5df7788a01af068a6f3397d497e2aebe39dd
prachisuryawanshi31766 <suryawanshiprachi0405.com>
2024-12-09 11:15:37 +0530

    11:14 AM -9/12/2024 this is Vishnu's Dashavtar project

---
+++ lecture_23/02_Assignment/Vishnu_Dashavtar_Project/script.js
@@ -0,0 +1,13 @@
+
+
+const dropdownBtn = document.querySelector(".dropdown_button");
+const dropdownList = document.querySelector(".dropdown_list");
+
+function toggleDropdown() {
+    dropdownList.classList.toggle("hide_element");
+}
+
+dropdownBtn.addEventListener("click", function(event) {
+    event.stopPropagation();
+    toggleDropdown();
+});
