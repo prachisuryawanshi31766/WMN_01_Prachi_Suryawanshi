f99841d567bfa74cf86f90f79e3be5cde79fa7a3
prachisuryawanshi31766 <suryawanshiprachi0405.com>
2024-12-09 11:12:09 +0530

    11:11 AM -9/12/2024 introducing Array

---
+++ lecture_18/01_ClassCode/01_Assecing_Array/script.js
@@ -0,0 +1,38 @@
+
+
+let colours=["Red","Green","Blue"];
+
+//Declaration of Array  ------------> 1.let arr=new array();
+ //                     ------------->2.let arr=[];  
+
+
+
+
+
+let colors = ["Red","Green","Blue"];
+
+//Array element access
+
+        alert(typeof [])
+
+        alert(colors[0])
+        alert(colors[1])
+        alert(colors[2])
+        alert(colors[3])
+
+
+//An array (fruits) containing 8 elements.
+
+let fruit = ["mango","banana","apple","orange","strawberry","cherry","kivi","dragon fruit"];
+
+
+
+
+//Array named constants containing 5 floating values.
+
+let constants = [1.1 , 2.333 , 45.66 , 784.43 , 234.678];
+
+
+//Array named ages containing 10 unique age value.
+
+let ages =[12 , 18 , 19 , 45 , 23 , 55 , 21 , 51 , 75 , 100 ]; 
